619be70be7b35da997d4747c33005e373d64a6ed
rohit <rohit.excel2011@gmail.com>
2017-04-03 19:30:37 +0530

    remove console.log

--- services/regisuser.js
+++ services/regisuser.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export function setuserinfo(info, id, name, userEmail, gender, device_id) {
-    console.log(info, id, name, userEmail, gender, device_id);
     return new Promise(function(resolve, reject) {
         axios.get(CONF.baseURL + info + "&id=" + id + "&name=" + name + "&email=" + userEmail + "&gender=" + gender + "&device_id=" + device_id).then((val) => {
             resolve(val)
@@ -11,10 +10,8 @@ export function setuserinfo(info, id, name, userEmail, gender, device_id) {
     })
 }
 export function setuserkey(device_id, user_key, fcm_reg_id) {
-    console.log(device_id, user_key, fcm_reg_id);
     return new Promise(function(resolve, reject) {
         axios.get(ADDUSER.baseURL + device_id + "&user_key=" + user_key + "&gcm_reg_id=" + fcm_reg_id).then((val) => {
-            console.log(val);
             resolve(val)
         }, (error) => {
             reject({error})
