8ede02ed585247a3c015ea8a3b72b19164592f9a
excellencetechnologies <excellencetechnologies08@gmail.com>
2017-05-30 10:21:39 +0530

    app

--- services/product.js
+++ services/product.js
@@ -2,7 +2,6 @@ import * as actions from './ajaxfire';
 export function getProduct(name, id, sub_id, page, popularity) {
   return new Promise((resolve, reject) => {
     actions.ajaxFire(`all_products&actual_link=log/xxx/${id}/${sub_id}/json__true=&id=${id}&sub_id=${sub_id}&category=${name}&json=true&cat_id=${id}&sub_cat_id=${sub_id}&sort=${popularity}&page=${page}`).then((val) => {
-      console.log(val,'test');
       const product = val.data;
       setLocalStorageData('Api_data', JSON.stringify(val.data));
       setLocalStorageData('filter_brands', JSON.stringify(val.data.brands));
@@ -14,7 +13,6 @@ export function getProduct(name, id, sub_id, page, popularity) {
       }
       resolve(product);
     }, (error) => {
-      console.log(error,'asd');
       reject(error);
     });
   });
